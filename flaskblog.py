63e77775752bcf40294354e3c7e1c0c1ae9555b5
mubashir98 <mubashirbaig98@gmail.com>
2019-07-29 13:06:21 +0500

    2nd modification

--- flaskblog.py
+++ flaskblog.py
@@ -1,6 +1,9 @@
-from flask import Flask, render_template
-
+from flask import Flask, render_template, url_for,flash,redirect
+from forms import RegistrationForm,LoginForm
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = '79bed464d7561bddbc1f780af2d1ca13'
+
 posts=[
     {
         'author': 'Mubashir',
@@ -23,5 +26,25 @@ def home():
 @app.route("/about")
 def info():
     return render_template('about.html',title='About')
+
+@app.route("/register",methods=['GET','POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}!','success')
+        return redirect(url_for('home'))
+    return render_template('register.html', title='Register', form=form)
+
+@app.route("/login",methods=['GET','POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == 'admin@blog.com' and form.password.data == 'password':
+            flash('You have been logged in!', 'success')
+            return redirect((url_for('home')))
+        else:
+            flash('Login Unsuccessful, Please check username and password', 'danger')
+    return render_template('login.html', title='Login', form=form)
+
 if __name__ == '__main__':
     app.run(debug=True)
